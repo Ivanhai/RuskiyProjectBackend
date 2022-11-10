4f18b25721c5d77f580641d7a2757c220435b453
SDERFET <arche0730@gmail.com>
2022-11-10 18:20:35 +0700

    u

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 import spacy
+import uvicorn
 
 
 nlp = spacy.load("ru_core_news_md")
@@ -10,3 +11,6 @@ app = FastAPI()
 async def root(sentence):
     doc = nlp(sentence)
     return doc.to_json()
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True, log_level="debug")
