c4dbff758bdf97ebce51efb88be39e50c217ba2f
bot <12>
2022-12-06 16:51:04 +0000

    token text

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ async def root(sentence):
     json = doc.to_json()
     for index, token in enumerate(doc):
         json["tokens"][index]["morph"] = token.morph.to_dict()
+        json["tokens"][index]["text"] = token.text
     return json
 
 if __name__ == "__main__":
